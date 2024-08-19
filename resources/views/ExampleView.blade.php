33982b484126f87bdb6737471b7cc07dea80bc9e
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2024-08-19 12:38:59 -0300

    nao sei

--- resources/views/ExampleView.blade.php
+++ resources/views/ExampleView.blade.php
@@ -1,19 +1,17 @@
-@extends('layouts.main')
+@extends('layouts.LayoutExample')
 
 @section('styles')
-    <style>
-        .container {
-            display: flex;
-            justify-content: center;
-            height: 100vh;
-            padding: 1rem;
-        }
-    </style>
-
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+    }
+</style>
 @endsection
 
 @section('content')
-            <div class="container">
-                Example View
-            </div>
+<div class="container">
+    Example View
+</div>
 @endsection
--- resources/views/layouts/LayoutExample.blade.php
+++ resources/views/layouts/LayoutExample.blade.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <style>
-    header, footer {
+    header,
+    footer {
         background-color: #f1f1f1;
         padding: 1em;
         text-align: center;
     }
+
     footer {
         position: fixed;
         bottom: 0;
         width: 100%;
     }
-    h1, h2, h3 {
+
+    h1,
+    h2,
+    h3 {
         border-bottom: 1px solid #ccc;
     }
+
     body {
         margin: 0;
         font-family: Arial, sans-serif;
