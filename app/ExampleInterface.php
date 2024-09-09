35c032f024ce3845e6fcc9de950a639d58cebd9b
Matheus Lopes Duarte <131881614+matheuslopesduarte@users.noreply.github.com>
2024-09-09 12:11:50 +0000

    teste

--- app/ExampleInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-interface ExampleInterface
-{
-    // é tipo o interface do TypeScript ou Java, ou seja, burocrático e chato
-    // não utilizaremos muito isso no projeto por enquanto (ou nunca)
-}
